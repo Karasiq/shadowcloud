9101a1da1772b3568b9ffa0fb028d6094b457cb6
Karasiq <yoba123@yandex.ru>
2017-05-25 08:17:28 +0300

    ECIES fixes

--- crypto/bouncycastle/src/main/scala/com/karasiq/shadowcloud/crypto/bouncycastle/asymmetric/ECIESCipherModule.scala
+++ crypto/bouncycastle/src/main/scala/com/karasiq/shadowcloud/crypto/bouncycastle/asymmetric/ECIESCipherModule.scala
@@ -3,28 +3,53 @@ package com.karasiq.shadowcloud.crypto.bouncycastle.asymmetric
 import java.security.SecureRandom
 
 import scala.language.postfixOps
-import scala.util.control.NonFatal
 
 import akka.util.ByteString
 import com.typesafe.config.ConfigValueFactory
-import org.bouncycastle.crypto.KeyEncoder
+import org.bouncycastle.crypto.{Digest, KeyEncoder}
 import org.bouncycastle.crypto.agreement.ECDHBasicAgreement
 import org.bouncycastle.crypto.engines.IESEngine
 import org.bouncycastle.crypto.generators.{EphemeralKeyPairGenerator, KDF2BytesGenerator}
 import org.bouncycastle.crypto.macs.HMac
 import org.bouncycastle.crypto.params.{AsymmetricKeyParameter, ECPublicKeyParameters, IESParameters}
 import org.bouncycastle.crypto.parsers.ECIESPublicKeyParser
-import org.bouncycastle.crypto.util.DigestFactory
 
 import com.karasiq.shadowcloud.config.ConfigProps
-import com.karasiq.shadowcloud.crypto.{EncryptionMethod, EncryptionParameters, StreamEncryptionModule}
+import com.karasiq.shadowcloud.config.utils.ConfigImplicits
+import com.karasiq.shadowcloud.crypto.{AsymmetricEncryptionParameters, EncryptionMethod, EncryptionParameters, StreamEncryptionModule}
+import com.karasiq.shadowcloud.crypto.bouncycastle.hashing.{DigestWrapper, MessageDigestModule}
 import com.karasiq.shadowcloud.crypto.bouncycastle.internal.ECUtils
 import com.karasiq.shadowcloud.crypto.bouncycastle.sign.BCECKeys
 import com.karasiq.shadowcloud.utils.HexString
 
-private[bouncycastle] object ECIESCipherModule {
+private[bouncycastle] object ECIESCipherModule extends ConfigImplicits {
+  private[this] val secureRandom = new SecureRandom()
+
   def apply(method: EncryptionMethod = EncryptionMethod("ECIES")): ECIESCipherModule = {
     new ECIESCipherModule(method)
+  }
+
+  private def getDigest(method: EncryptionMethod): Digest = {
+    val digestName = ConfigProps.toConfig(method.config).withDefault("SHA1", _.getString("ies-digest"))
+    DigestWrapper(MessageDigestModule(digestName).messageDigest)
+  }
+
+  private def getIesParameters(method: EncryptionMethod): IESParameters = {
+    val config = ConfigProps.toConfig(method.config)
+    def bytesOrNull(name: String): Array[Byte] = config.withDefault(null, cfg ⇒ HexString.decode(cfg.getString(name)).toArray)
+    new IESParameters(bytesOrNull("ies-derivation"), bytesOrNull("ies-encoding"), config.withDefault(64, _.getInt("ies-mac-key-size")))
+  }
+
+  private def createIesParameters(parameters: AsymmetricEncryptionParameters): AsymmetricEncryptionParameters = {
+    def randomBytes(size: Int): String = {
+      val bytes = new Array[Byte](size)
+      secureRandom.nextBytes(bytes)
+      HexString.encode(ByteString(bytes))
+    }
+    val newConfig = ConfigProps.toConfig(parameters.method.config)
+      .withValue("ies-derivation", ConfigValueFactory.fromAnyRef(randomBytes(8)))
+      .withValue("ies-encoding", ConfigValueFactory.fromAnyRef(randomBytes(8)))
+    parameters.copy(method = parameters.method.copy(config = ConfigProps.fromConfig(newConfig)))
   }
 }
 
@@ -32,11 +57,9 @@ private[bouncycastle] final class ECIESCipherModule(val method: EncryptionMethod
   // TODO: Block cipher mode
   private[this] val cipher = new IESEngine(
     new ECDHBasicAgreement(),
-    new KDF2BytesGenerator(DigestFactory.createSHA1()),
-    new HMac(DigestFactory.createSHA1())
+    new KDF2BytesGenerator(ECIESCipherModule.getDigest(method)),
+    new HMac(ECIESCipherModule.getDigest(method))
   )
-
-  private[this] val secureRandom = new SecureRandom()
 
   //noinspection ConvertExpressionToSAM
   private[this] val ephKeyGen = new EphemeralKeyPairGenerator(keyPairGenerator, new KeyEncoder {
@@ -46,30 +69,12 @@ private[bouncycastle] final class ECIESCipherModule(val method: EncryptionMethod
   })
 
   override def createParameters(): EncryptionParameters = {
-    def generateHexString(size: Int): String = {
-      val bytes = new Array[Byte](size)
-      secureRandom.nextBytes(bytes)
-      HexString.encode(ByteString(bytes))
-    }
-    val parameters = super.createParameters().asymmetric
-    val newConfig = ConfigProps.toConfig(parameters.method.config)
-      .withValue("ies-derivation", ConfigValueFactory.fromAnyRef(generateHexString(8)))
-      .withValue("ies-encoding", ConfigValueFactory.fromAnyRef(generateHexString(8)))
-    parameters.copy(method = parameters.method.copy(config = ConfigProps.fromConfig(newConfig)))
+    val basicParameters = super.createParameters().asymmetric
+    ECIESCipherModule.createIesParameters(basicParameters)
   }
 
   def init(encrypt: Boolean, parameters: EncryptionParameters): Unit = {
-    import com.karasiq.shadowcloud.config.utils.ConfigImplicits._
-    require(cipher.ne(null), "No IES engine")
-    val config = ConfigProps.toConfig(parameters.method.config)
-    def readHexString(name: String): Array[Byte] = {
-      try {
-        HexString.decode(config.getString(name)).toArray
-      } catch { case NonFatal(_) ⇒
-        null
-      }
-    }
-    val params = new IESParameters(readHexString("ies-derivation"), readHexString("ies-encoding"), config.withDefault(64, _.getInt("ies-mac-key-size")))
+    val params = ECIESCipherModule.getIesParameters(parameters.method)
     val publicKey = getCipherKey(parameters, encrypt)
     if (encrypt) {
       cipher.init(publicKey, params, ephKeyGen)

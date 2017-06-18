0b07ec6e5018f08ef100b9c85f230e447b33cc06
Karasiq <yoba123@yandex.ru>
2017-06-18 19:49:52 +0300

    JavaFX dialog fixes

--- javafx/src/main/scala/com/karasiq/shadowcloud/javafx/JavaFXPasswordProvider.scala
+++ javafx/src/main/scala/com/karasiq/shadowcloud/javafx/JavaFXPasswordProvider.scala
@@ -5,8 +5,10 @@ import scala.concurrent.duration.Duration
 import scalafx.application.Platform
 import scalafx.scene.control.{ButtonType, Dialog, PasswordField}
 import scalafx.scene.control.ButtonBar.ButtonData
-import scalafx.scene.layout.GridPane
+import scalafx.scene.layout.{GridPane, Priority}
 import scalafx.Includes._
+import scalafx.geometry.Insets
+import scalafx.scene.image.ImageView
 
 import akka.actor.ActorSystem
 
@@ -17,19 +19,50 @@ private[javafx] final class JavaFXPasswordProvider(actorSystem: ActorSystem) ext
   private[this] val context = JavaFXContext(actorSystem)
 
   private[this] def showPasswordDialog(passwordId: String): Option[String] = {
-    val dialog: Dialog[String] = new Dialog[String] {
-      initOwner(context.app.stage)
+    val dialog = new PasswordDialog(passwordId)
+    dialog.initOwner(context.app.stage)
+    dialog.showAndWait().filter(_.nonEmpty)
+  }
 
+  def askPassword(id: String): String = {
+    val promise = Promise[String]
+    Platform.runLater {
+      showPasswordDialog(id) match {
+        case Some(password) ⇒
+          promise.success(password)
+
+        case None ⇒
+          promise.failure(new IllegalArgumentException("No password provided"))
+      }
+    }
+    Await.result(promise.future, Duration.Inf)
+  }
+
+  private[this] class PasswordDialog(passwordId: String) extends Dialog[String] {
+    def showAndWait(): Option[String] = {
+      showAndWait((s: String) ⇒ s).asInstanceOf[Option[String]]
+    }
+
+    private[this] def getResourcePath(fileName: String) = {
+      getClass.getClassLoader.getResource(fileName).toString
+    }
+
+    private[this] def initDialog(): Unit = {
       val passwordField = new PasswordField {
+        hgrow = Priority.Always
         promptText = "Password"
       }
 
-      val grid = new GridPane {
+      val fieldGrid = new GridPane {
+        hgap = 10
+        vgap = 10
+        padding = Insets(20, 10, 10, 10)
         add(passwordField, 0, 0)
       }
 
       title = "Password dialog"
-      headerText = s"Enter password ($passwordId): "
+      headerText = s"Enter $passwordId password"
+      graphic = new ImageView(getResourcePath("key-icon_32.png"))
 
       val okButtonType = new ButtonType("OK", ButtonData.OKDone)
       dialogPane().buttonTypes += okButtonType
@@ -46,23 +79,10 @@ private[javafx] final class JavaFXPasswordProvider(actorSystem: ActorSystem) ext
           ""
       }
 
-      dialogPane().content = grid
+      dialogPane().content = fieldGrid
       Platform.runLater(passwordField.requestFocus())
     }
-    dialog.showAndWait().asInstanceOf[Option[String]].filter(_.nonEmpty)
-  }
 
-  def askPassword(id: String): String = {
-    val promise = Promise[String]
-    Platform.runLater {
-      showPasswordDialog(id) match {
-        case Some(password) ⇒
-          promise.success(password)
-
-        case None ⇒
-          promise.failure(new IllegalArgumentException("No password provided"))
-      }
-    }
-    Await.result(promise.future, Duration.Inf)
+    initDialog()
   }
 }

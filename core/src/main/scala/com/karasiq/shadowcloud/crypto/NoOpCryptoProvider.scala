ca0664a0cc25061b8c50aa0082ed711eaaa86777
Karasiq <yoba123@yandex.ru>
2017-08-02 22:58:23 +0300

    Crypto fixes

--- core/src/main/scala/com/karasiq/shadowcloud/crypto/NoOpCryptoProvider.scala
+++ core/src/main/scala/com/karasiq/shadowcloud/crypto/NoOpCryptoProvider.scala
@@ -2,19 +2,30 @@ package com.karasiq.shadowcloud.crypto
 
 import scala.language.postfixOps
 
+import com.typesafe.config.Config
+
+import com.karasiq.shadowcloud.config.utils.ConfigImplicits
 import com.karasiq.shadowcloud.crypto.internal.{NoOpEncryptionModule, NoOpHashingModule, NoOpSignModule}
 import com.karasiq.shadowcloud.providers.CryptoProvider
 
-private[crypto] final class NoOpCryptoProvider extends CryptoProvider {
+private[crypto] final class NoOpCryptoProvider(config: Config) extends CryptoProvider {
+  private[this] object settings extends ConfigImplicits {
+    val requireEncryption: Boolean = config.withDefault(true, _.getBoolean("crypto.no-op.require-encryption"))
+  }
+
   override def hashing: HashingPF = {
     case m if m.algorithm.isEmpty ⇒ new NoOpHashingModule
   }
 
   override def encryption: EncryptionPF = {
-    case m if m.algorithm.isEmpty ⇒ new NoOpEncryptionModule
+    case m if m.algorithm.isEmpty ⇒
+      require(!settings.requireEncryption, "Valid encryption is required")
+      new NoOpEncryptionModule
   }
 
   override def signing: SignPF = {
-    case m if m.algorithm.isEmpty ⇒ new NoOpSignModule
+    case m if m.algorithm.isEmpty ⇒
+      require(!settings.requireEncryption, "Valid signature is required")
+      new NoOpSignModule
   }
 }

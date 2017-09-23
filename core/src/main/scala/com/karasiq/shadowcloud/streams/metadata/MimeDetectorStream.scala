66b067b15ee08b49e42217c0d29f40193570ad20
Karasiq <yoba123@yandex.ru>
2017-09-23 03:43:12 +0300

    Mime fixes

--- core/src/main/scala/com/karasiq/shadowcloud/streams/metadata/MimeDetectorStream.scala
+++ core/src/main/scala/com/karasiq/shadowcloud/streams/metadata/MimeDetectorStream.scala
@@ -10,44 +10,48 @@ import com.karasiq.shadowcloud.metadata.MimeDetector
 
 object MimeDetectorStream {
   def apply(detector: MimeDetector, fileName: String, probeSize: Int): Flow[ByteString, String, NotUsed] = {
+    val defaultMimeSource = Source.single(MimeDetector.DefaultMime)
     Flow.fromGraph(new MimeDetectorStream(detector, fileName, probeSize))
-      .recoverWithRetries(1, { case _ ⇒ Source.single(MimeDetector.DefaultMime) })
-      .orElse(Source.single(MimeDetector.DefaultMime))
-      .take(1)
+      .recoverWithRetries(1, { case _ ⇒ defaultMimeSource })
+      .orElse(defaultMimeSource)
   }
 }
 
 private final class MimeDetectorStream(detector: MimeDetector, fileName: String, probeSize: Int)
   extends GraphStage[FlowShape[ByteString, String]] {
 
-  val inlet = Inlet[ByteString]("MimeDetectorStream.in")
-  val outlet = Outlet[String]("MimeDetectorStream.out")
-  val shape = FlowShape(inlet, outlet)
+  val inBytes = Inlet[ByteString]("MimeDetectorStream.in")
+  val outContentType = Outlet[String]("MimeDetectorStream.out")
+  val shape = FlowShape(inBytes, outContentType)
 
   def createLogic(inheritedAttributes: Attributes) = new GraphStageLogic(shape) with InHandler with OutHandler {
     private[this] var buffer = ByteString.empty
 
     private[this] def pushContentType(): Unit = {
-      if (buffer.length >= probeSize || isClosed(inlet)) {
-        val contentType = detector.getMimeType(fileName, buffer.take(probeSize))
+      if (buffer.length >= probeSize || isClosed(inBytes)) {
+        val contentType = detector.getMimeType(fileName, buffer/*.take(probeSize)*/)
         buffer = ByteString.empty
-        push(outlet, contentType.getOrElse(MimeDetector.DefaultMime))
+        contentType.foreach(push(outContentType, _))
         completeStage()
       } else {
-        tryPull(inlet)
+        tryPull(inBytes)
       }
     }
 
     def onPull(): Unit = {
-      tryPull(inlet)
+      tryPull(inBytes)
     }
 
     def onPush(): Unit = {
-      val element = grab(inlet)
+      val element = grab(inBytes)
       buffer ++= element
       pushContentType()
     }
 
-    setHandlers(inlet, outlet, this)
+    override def onUpstreamFinish(): Unit = {
+      pushContentType()
+    }
+
+    setHandlers(inBytes, outContentType, this)
   }
 }

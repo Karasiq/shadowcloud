6f88f721d475428dac52606b9068846e76cef56b
Karasiq <yoba123@yandex.ru>
2017-09-23 03:27:29 +0300

    Upload queue

--- server/webapp/src/main/scala/com/karasiq/shadowcloud/webapp/components/folder/UploadForm.scala
+++ server/webapp/src/main/scala/com/karasiq/shadowcloud/webapp/components/folder/UploadForm.scala
@@ -3,7 +3,8 @@ package com.karasiq.shadowcloud.webapp.components.folder
 import com.karasiq.bootstrap.Bootstrap.default._
 import scalaTags.all._
 
-import rx.Rx
+import org.scalajs.dom
+import rx.{Rx, Var}
 
 import com.karasiq.shadowcloud.model.Path
 import com.karasiq.shadowcloud.webapp.components.common.{AppComponents, AppIcons}
@@ -19,27 +20,16 @@ object UploadForm {
 class UploadForm(implicit appContext: AppContext, folderContext: FolderContext) extends BootstrapHtmlComponent {
   import folderContext.{regionId, selected ⇒ selectedFolderPathRx}
   private[this] val uploadProgressBars = div().render
+  private[this] val uploadQueue = Var(List.empty[dom.File])
+  private[this] val uploading = Var(List.empty[dom.File])
+
+  uploadQueue.triggerLater(processQueue())
+  uploading.triggerLater(processQueue())
 
   def renderTag(md: ModifierT*): TagT = {
     val uploadInput = FormInput.file(appContext.locale.file, multiple, md, onchange := Callback.onInput { input ⇒
-      input.files.foreach { inputFile ⇒
-        val parent: Path = selectedFolderPathRx.now
-        val (progressRx, fileFuture) = appContext.api.uploadFile(regionId, parent / inputFile.name, inputFile)
-        val progressBar = renderProgressBar(inputFile.name, progressRx).render
-
-        uploadProgressBars.appendChild(progressBar)
-        fileFuture.onComplete { _ ⇒
-          // progressBar.outerHTML = ""
-          uploadProgressBars.removeChild(progressBar)
-          progressRx.kill()
-        }
-
-        fileFuture.foreach { file ⇒
-          folderContext.update(parent)
-          // dom.window.alert(file.toString)
-          input.form.reset()
-        }
-      }
+      uploadQueue() = uploadQueue.now ++ input.files.toList
+      input.form.reset()
     })
 
     div(
@@ -65,6 +55,31 @@ class UploadForm(implicit appContext: AppContext, folderContext: FolderContext) 
       ProgressBar.withLabel(progress).renderTag(styles:_*),
       hr
     )
+  }
+
+  private[this] def processQueue() = {
+    val toUploadN = 5 - uploading.now.length
+    val (toUpload, rest) = uploadQueue.now
+      .filterNot(uploading.now.contains)
+      .splitAt(toUploadN)
+
+    toUpload.foreach { file ⇒
+      val parent: Path = selectedFolderPathRx.now
+      val (progressRx, fileFuture) = appContext.api.uploadFile(regionId, parent / file.name, file)
+      val progressBar = renderProgressBar(file.name, progressRx).render
+
+      uploadProgressBars.appendChild(progressBar)
+      fileFuture.onComplete { _ ⇒
+        uploading() = uploading.now.filterNot(_ == file)
+        uploadProgressBars.removeChild(progressBar)
+        progressRx.kill()
+      }
+
+      fileFuture.foreach(_ ⇒ folderContext.update(parent))
+    }
+
+    uploading() = uploading.now ++ toUpload
+    uploadQueue() = rest
   }
 }
 

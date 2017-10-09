37433728a9e86d98dae7524a4acd1f2bc0c22b97
Karasiq <yoba123@yandex.ru>
2017-10-09 19:15:47 +0300

    Remove test code

--- server/src/main/scala/com/karasiq/shadowcloud/server/http/HttpServerMain.scala
+++ server/src/main/scala/com/karasiq/shadowcloud/server/http/HttpServerMain.scala
@@ -1,7 +1,5 @@
 package com.karasiq.shadowcloud.server.http
 
-import java.nio.file.{Files, Paths}
-
 import scala.language.postfixOps
 
 import akka.actor.ActorSystem
@@ -11,20 +9,19 @@ import akka.http.scaladsl.settings.ServerSettings
 import com.typesafe.config.ConfigFactory
 
 import com.karasiq.shadowcloud.ShadowCloud
-import com.karasiq.shadowcloud.storage.props.StorageProps
 
 object HttpServerMain extends HttpApp with App with PredefinedToResponseMarshallers with SCAkkaHttpServer {
-  // Actor system
+  // -----------------------------------------------------------------------
+  // Context
+  // -----------------------------------------------------------------------
   private[this] val config = {
     val defaultConfig = ConfigFactory.load()
     val serverAppConfig = ConfigFactory.load("sc-server-app").withFallback(defaultConfig)
     serverAppConfig
   }
+
   private[this] val actorSystem: ActorSystem = ActorSystem("shadowcloud-server", config)
   protected val sc = ShadowCloud(actorSystem)
-
-  import sc.implicits._
-  import sc.ops.supervisor
 
   // -----------------------------------------------------------------------
   // Route
@@ -44,30 +41,7 @@ object HttpServerMain extends HttpApp with App with PredefinedToResponseMarshall
   }
 
   // -----------------------------------------------------------------------
-  // Pre-start
-  // -----------------------------------------------------------------------
-  sc.keys.getOrGenerateChain().foreach { keyChain ⇒
-    println(s"Key chain initialized: $keyChain")
-  }
-
-  val tempDirectory = sys.props.get("shadowcloud.temp-storage-dir")
-    .map(Paths.get(_))
-    .getOrElse(Files.createTempDirectory("scl-temp-storage"))
-
-  Files.createDirectories(tempDirectory.resolve("second"))
-  supervisor.createRegion("testRegion", sc.configs.regionConfig("testRegion"))
-  supervisor.createStorage("testStorage", StorageProps.fromDirectory(tempDirectory))
-  supervisor.createStorage("testStorage2", StorageProps.fromDirectory(tempDirectory.resolve("second")))
-  supervisor.register("testRegion", "testStorage")
-  supervisor.register("testRegion", "testStorage2")
-
-  /* import scala.concurrent.duration._
-  actorSystem.scheduler.scheduleOnce(30 seconds) {
-    sc.ops.region.collectGarbage("testRegion", delete = true)
-  } */
-
-  // -----------------------------------------------------------------------
   // Start server
   // -----------------------------------------------------------------------
-  startServer("0.0.0.0", 9000, ServerSettings(actorSystem), actorSystem)
+  startServer(SCHttpSettings.host, SCHttpSettings.port, ServerSettings(actorSystem), actorSystem)
 }
--- server/src/main/scala/com/karasiq/shadowcloud/server/http/SCHttpServerSettings.scala
+++ server/src/main/scala/com/karasiq/shadowcloud/server/http/SCHttpServerSettings.scala
@@ -11,6 +11,8 @@ trait SCHttpServerSettings {
   // -----------------------------------------------------------------------
   protected object SCHttpSettings extends ConfigImplicits {
     val config = sc.config.rootConfig.getConfig("http-server")
+    val host = config.withDefault("127.0.0.1", _.getString("host"))
+    val port = config.withDefault(1911, _.getInt("port"))
     val useMultipartByteRanges = config.getBoolean("use-multipart-byte-ranges")
   }
 }

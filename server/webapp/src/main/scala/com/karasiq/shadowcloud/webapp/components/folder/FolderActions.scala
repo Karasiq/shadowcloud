1f49cd55b3452d62744aac5d7ee1daec24cf828c
Karasiq <yoba123@yandex.ru>
2017-12-31 22:23:21 +0300

    Rename folder

--- server/webapp/src/main/scala/com/karasiq/shadowcloud/webapp/components/folder/FolderActions.scala
+++ server/webapp/src/main/scala/com/karasiq/shadowcloud/webapp/components/folder/FolderActions.scala
@@ -8,19 +8,19 @@ import rx.Var
 import com.karasiq.shadowcloud.model.{Path, RegionId}
 import com.karasiq.shadowcloud.webapp.components.common.{AppComponents, AppIcons}
 import com.karasiq.shadowcloud.webapp.components.folder.FolderTree.FolderController
-import com.karasiq.shadowcloud.webapp.context.AppContext
+import com.karasiq.shadowcloud.webapp.context.{AppContext, FolderContext}
 import AppContext.JsExecutionContext
 
 private[folder] object FolderActions {
   def apply(regionId: RegionId, path: Path)
-           (implicit context: AppContext, fc: FolderController): FolderActions = {
+           (implicit appContext: AppContext, folderContext: FolderContext, folderController: FolderController): FolderActions = {
 
     new FolderActions(regionId, path)
   }
 }
 
 private[folder] class FolderActions(regionId: RegionId, path: Path)
-                                   (implicit context: AppContext, fc: FolderController) extends BootstrapHtmlComponent {
+                                   (implicit context: AppContext, folderContext: FolderContext, folderController: FolderController) extends BootstrapHtmlComponent {
 
   def renderTag(md: ModifierT*): TagT = {
     def showCreateFolderModal(): Unit = {
@@ -32,7 +32,29 @@ private[folder] class FolderActions(regionId: RegionId, path: Path)
         .withBody(Form(FormInput.text(context.locale.name, folderNameRx.reactiveInputRead)))
         .withButtons(
           Modal.button(context.locale.submit, Modal.dismiss, isFolderNameValid.reactiveShow, onclick := Callback.onClick { _ ⇒
-            context.api.createFolder(regionId, path / folderNameRx.now).foreach(fc.addFolder)
+            context.api.createFolder(regionId, path / folderNameRx.now).foreach(folderController.addFolder)
+          }),
+          AppComponents.modalClose()
+        )
+        .show()
+    }
+
+    def showRenameFolderModal(): Unit = {
+      val folderNameRx = Var("")
+      val isFolderNameValid = folderNameRx.map(_.nonEmpty)
+
+      Modal()
+        .withTitle(context.locale.rename)
+        .withBody(Form(FormInput.text(context.locale.name, folderNameRx.reactiveInputRead)))
+        .withButtons(
+          Modal.button(context.locale.submit, Modal.dismiss, isFolderNameValid.reactiveShow, onclick := Callback.onClick { _ ⇒
+            for {
+              newFolder ← context.api.copyFolder(regionId, path, path.withName(folderNameRx.now), folderContext.scope.now)
+              oldFolder ← context.api.deleteFolder(regionId, path)
+            } {
+              folderController.deleteFolder(oldFolder)
+              folderController.addFolder(newFolder)
+            }
           }),
           AppComponents.modalClose()
         )
@@ -45,7 +67,7 @@ private[folder] class FolderActions(regionId: RegionId, path: Path)
         .withBody(context.locale.deleteFolderConfirmation(path))
         .withButtons(
           AppComponents.modalSubmit(onclick := Callback.onClick { _ ⇒
-            context.api.deleteFolder(regionId, path).foreach(fc.deleteFolder)
+            context.api.deleteFolder(regionId, path).foreach(folderController.deleteFolder)
           }),
           AppComponents.modalClose()
         )
@@ -57,8 +79,9 @@ private[folder] class FolderActions(regionId: RegionId, path: Path)
     }
 
     span(
-      renderIcon(AppIcons.refresh, fc.update),
+      // renderIcon(AppIcons.refresh, fc.update),
       renderIcon(AppIcons.create, showCreateFolderModal),
+      if (!path.isRoot) renderIcon(AppIcons.rename, showRenameFolderModal) else (),
       if (!path.isRoot) renderIcon(AppIcons.delete, showDeleteFolderModal) else (),
       md
     )

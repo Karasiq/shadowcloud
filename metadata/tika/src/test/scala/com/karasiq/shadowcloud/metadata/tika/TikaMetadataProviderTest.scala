e2abb1daa33378e6bdcf6e89b644de1816161e3c
Karasiq <yoba123@yandex.ru>
2018-02-05 13:43:23 +0300

    Test fixes

--- metadata/tika/src/test/scala/com/karasiq/shadowcloud/metadata/tika/TikaMetadataProviderTest.scala
+++ metadata/tika/src/test/scala/com/karasiq/shadowcloud/metadata/tika/TikaMetadataProviderTest.scala
@@ -10,12 +10,12 @@ import akka.stream.testkit.scaladsl.TestSink
 import akka.util.ByteString
 import org.apache.commons.io.FileUtils
 import org.apache.tika.Tika
-import org.scalatest.FlatSpecLike
+import org.scalatest.{FlatSpecLike, SequentialNestedSuiteExecution}
 
 import com.karasiq.shadowcloud.metadata.Metadata
 import com.karasiq.shadowcloud.test.utils.{ActorSpec, ActorSpecImplicits}
 
-class TikaMetadataProviderTest extends ActorSpec with ActorSpecImplicits with FlatSpecLike {
+class TikaMetadataProviderTest extends ActorSpec with ActorSpecImplicits with FlatSpecLike with SequentialNestedSuiteExecution {
   val testPdfName = "TypeClasses.pdf"
   val testPdfFile = new File(getClass.getClassLoader.getResource(testPdfName).toURI)
   val testPdfBytes = ByteString(FileUtils.readFileToByteArray(testPdfFile))
@@ -31,29 +31,29 @@ class TikaMetadataProviderTest extends ActorSpec with ActorSpecImplicits with Fl
   val autoParser = TikaAutoParser(tika, autoParserConfig)
 
   "Parser" should "extract text" in {
-    val output = FileIO.fromPath(testPdfFile.toPath)
+    val stream = FileIO.fromPath(testPdfFile.toPath)
       .via(autoParser.parseMetadata(testPdfName, "application/pdf"))
       .toMat(TestSink.probe)(Keep.right)
       .run()
 
-    val metaTable = output.requestNext()
+    val metaTable = stream.requestNext(1 minute)
     metaTable.tag shouldBe Some(Metadata.Tag("tika", "auto", Metadata.Tag.Disposition.METADATA))
     println(metaTable)
     assert(metaTable.value.table.exists(_.values("dcterms:created").values == Seq("2010-07-26T09:01:12Z")))
 
-    val textPreview = output.requestNext()
+    val textPreview = stream.requestNext(1 minute)
     textPreview.tag shouldBe Some(Metadata.Tag("tika", "auto", Metadata.Tag.Disposition.PREVIEW))
     textPreview.value.text.exists(t ⇒ t.format == "text/plain" && t.data.contains("Type Classes as Objects and Implicits")) shouldBe true
 
-    val text = output.requestNext(1 minute)
+    val text = stream.requestNext(1 minute)
     text.tag shouldBe Some(Metadata.Tag("tika", "auto", Metadata.Tag.Disposition.CONTENT))
     text.value.text.exists(t ⇒ t.format == "text/plain" && t.data.contains("Type Classes as Objects and Implicits")) shouldBe true
 
-    val xml = output.requestNext()
+    val xml = stream.requestNext()
     xml.tag shouldBe Some(Metadata.Tag("tika", "auto", Metadata.Tag.Disposition.CONTENT))
     xml.value.text.exists(t ⇒ t.format == "text/html" && t.data.contains("<p>Adriaan Moors Martin Odersky\nEPFL\n</p>")) shouldBe true
 
-    output.request(1)
-    output.expectComplete()
+    stream.request(1)
+    stream.expectComplete()
   }
 }

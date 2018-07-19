e41ce7937a22770e8e30a5e17e0d8caf49289cf9
Karasiq <yoba123@yandex.ru>
2018-07-19 14:35:13 +0300

    Tests fix

--- core/src/test/scala/com/karasiq/shadowcloud/test/utils/SCExtensionSpec.scala
+++ core/src/test/scala/com/karasiq/shadowcloud/test/utils/SCExtensionSpec.scala
@@ -6,4 +6,9 @@ import com.karasiq.shadowcloud.{ShadowCloud, ShadowCloudExtension}
 
 abstract class SCExtensionSpec extends ActorSpec with ActorSpecImplicits with ByteStringImplicits {
   implicit val sc: ShadowCloudExtension = ShadowCloud(system)
+
+  override protected def beforeAll(): Unit = {
+    super.beforeAll()
+    sc.init()
+  }
 }

aef28bf0206dd47f2a87b21ac44aeca3e1dfdfbf
Karasiq <yoba123@yandex.ru>
2018-07-29 12:54:37 +0300

    Cache optimization

--- cache/larray/src/main/scala/com/github/karasiq/cache/larray/LArrayChunkCache.scala
+++ cache/larray/src/main/scala/com/github/karasiq/cache/larray/LArrayChunkCache.scala
@@ -8,9 +8,18 @@ import com.karasiq.shadowcloud.model.Chunk
 
 private[cache] class LArrayChunkCache(size: Int)(implicit ec: ExecutionContext) extends ChunkCache {
   protected val cache = LArrayAsyncLRUCache[Chunk](size)
+  protected var lastChunk: Chunk = _
 
   def readCached(chunk: Chunk, getChunk: () ⇒ Future[Chunk]): Future[Chunk] = {
+    // Single read optimization
+    val last = this.lastChunk
+    if (last == chunk) return Future.successful(last)
+
     val future = cache.getCached(chunk.withoutData, () ⇒ getChunk().map(_.data.plain))
-    future.map(bytes ⇒ chunk.copy(data = chunk.data.copy(plain = bytes)))
+    future.map { bytes ⇒
+      val chunkWithData = chunk.copy(data = chunk.data.copy(plain = bytes))
+      this.lastChunk = chunkWithData
+      chunkWithData
+    }
   }
 }

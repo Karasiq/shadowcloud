96974c1da18ef939b0c6a1a344313c137f66f1bd
Valeriy Gavrilin <yoba123@yandex.ru>
2019-11-19 18:59:58 +0300

    Fixes

--- build-package.sh
+++ build-package.sh
@@ -1,6 +1,6 @@
 export VERSION=1.0.2
 
 export "SBT_OPTS=-Xmx8G -Denable-tika=0 -Denable-javacv=0" && \
-  sbt desktopApp/universal:packageBin desktopApp/jdkPackager:packageBin && mv -f "./desktop-app/target/universal/shadowcloud-desktop-$VERSION.zip" "./desktop-app/target/universal/shadowcloud-$VERSION-light.zip" && \
+  sbt desktopApp/universal:packageBin && mv -f "./desktop-app/target/universal/shadowcloud-desktop-$VERSION.zip" "./desktop-app/target/universal/shadowcloud-$VERSION-light.zip" && \
   export SBT_OPTS=-"Xmx8G -Denable-tika=1 -Denable-javacv=1" && sbt desktopApp/jdkPackager:packageBin && export SBT_OPTS="-Xmx8G -Denable-tika=1 -Denable-javacv-all=1" && \
   sbt desktopApp/universal:packageBin && mv -f "./desktop-app/target/universal/shadowcloud-desktop-$VERSION.zip" "./desktop-app/target/universal/shadowcloud-$VERSION-full.zip"
